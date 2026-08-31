f1e6712aea9b00ea4487d864831e743b8f5a8d1d
Pavel Borisov <pavelborisov@Pavels-MacBook-Air-2.local>
2026-08-31 19:51:48 +0100

    Change main endpoint route to root

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,4 @@
-from fastapi import APIRouter
-
 from app.api.routers.dashboard import router as dashboard_router
 
 
-api_router = APIRouter(prefix="/api")
-api_router.include_router(dashboard_router)
+api_router = dashboard_router
